7483f9317d77b34ab02792168b329509c6f93028
chlorella71 <chlorella71@naver.com>
2024-10-02 10:23:34 +0900

    .

--- javadev/src/control/IfTest2.java
+++ javadev/src/control/IfTest2.java
@@ -1,13 +1,15 @@
 package control;
 
-public class IfTest1 {
+public class IfTest2 {
 	public static void main(String[] args) {
 		int n = 1;
 		
-		if (n==2) {
+		if (n==2 || n==3) {
 			System.out.println("M");
-		} else {
+		} else if (n==2 || n==4){
 			System.out.println("F");
+		} else {
+			System.out.println("E");
 		}
 		System.out.print("종료");
 	}
